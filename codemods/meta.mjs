3f1e6a5afa969ebfede14de574b2e1af73ed2738
Alistair Smith <hi@alistair.sh>
2022-09-02 14:17:20 +0200

    meta -> _meta

--- codemods/meta.mjs
+++ codemods/meta.mjs
@@ -1,7 +1,7 @@
-import {sync} from 'glob';
+import glob from 'glob';
 import {rename} from 'node:fs/promises';
 
-const metas = sync('./**/*/meta.json');
+const metas = glob.sync('./**/*/meta.json');
 
 for (const meta of metas) {
 	rename(meta, meta.replace('meta.json', '_meta.json'));
