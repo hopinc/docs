523eebe65a5c9cf2565429b31965dce24b4cd0c1
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-01-03 12:22:13 -0800

    [Docs] Add initial content and configuration

--- components/GradientDivider.tsx
+++
@@ -1,3 +0,0 @@
-export default function GradientDivider() {
-	return <div className="w-16 mt-5 h-0.5 bg-primary-500" />;
-}
--- components/label.tsx
+++
@@ -1,9 +0,0 @@
-import {PropsWithChildren} from 'react';
-
-export function Label({children}: PropsWithChildren<{}>) {
-	return (
-		<span className="ml-2 p-[6px] rounded-lg bg-red-500/50 text-white font-semibold uppercase text-lg align-middle">
-			{children}
-		</span>
-	);
-}
